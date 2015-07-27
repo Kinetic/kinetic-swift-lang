9726547e143d04ec5778a03f63ea2d0450c22cd5
Ignacio Corderi <icorderi@msn.com>
2015-07-27 23:02:23 +0200

    First commit

---
+++ Kinetic/Kinetic.h
@@ -0,0 +1,19 @@
+//
+//  Kinetic.h
+//  Kinetic
+//
+//  Created by Ignacio Corderi on 7/27/15.
+//  Copyright © 2015 Seagate. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Kinetic.
+FOUNDATION_EXPORT double KineticVersionNumber;
+
+//! Project version string for Kinetic.
+FOUNDATION_EXPORT const unsigned char KineticVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kinetic/PublicHeader.h>
+
+
